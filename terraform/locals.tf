55458850056986ce40db44ebf2ca608a9043d0ea
rafaelvastag <rgvastag@gmail.com>
2023-04-02 18:46:21 -0300

    alb

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,4 +1,6 @@
 locals {
+  subnet_ids = { for k, v in aws_subnet.this : v.tags.Name => v.id }
+
   common_tags = {
     Project   = "TODO Auto Scaling App"
     CreatedAt = timestamp()
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,23 +1,47 @@
 resource "aws_vpc" "this" {
   cidr_block = "192.168.0.0/16"
-  tags = merge(local.common_tags, { Name = "Terraform VPC"})
+  tags       = merge(local.common_tags, { Name = "Terraform VPC" })
 }
 
 resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
-  tags = merge(local.common_tags, { Name = "Terraform IGW"})
+  tags   = merge(local.common_tags, { Name = "Terraform IGW" })
 }
 
-resource "aws_subnet" "public_a" {
-  vpc_id = aws_vpc.this.id
-  cidr_block = "192.168.0.0/24"
-  availability_zone = "${var.aws_region}a"
-  tags = merge(local.common_tags, { Name = "Public A"})
+resource "aws_subnet" "this" {
+
+  for_each = {
+    "pub_a" = ["192.168.1.0/24", "${var.aws_region}a", "pub_a"]
+    "pub_b" = ["192.168.2.0/24", "${var.aws_region}b", "pub_b"]
+    "pvt_a" = ["192.168.3.0/24", "${var.aws_region}a", "pvt_a"]
+    "pvt_b" = ["192.168.4.0/24", "${var.aws_region}b", "pvt_b"]
+  }
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = each.value[0]
+  availability_zone = each.value[1]
+  tags              = merge(local.common_tags, { Name = each.value[2] })
 }
 
-resource "aws_subnet" "public_b" {
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
-  cidr_block = "192.168.1.0/24"
-  availability_zone = "${var.aws_region}b"
-  tags = merge(local.common_tags, { Name = "Public B"})
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = merge(local.common_tags, { Name = "Terraform Public" })
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+  tags   = merge(local.common_tags, { Name = "Terraform Private" })
+}
+
+resource "aws_route_table_association" "this" {
+  for_each = local.subnet_ids
+
+  subnet_id      = each.value
+  route_table_id = substr(each.key, 0, 3) == "pub" ? aws_route_table.public.id : aws_route_table.private.id
 }
